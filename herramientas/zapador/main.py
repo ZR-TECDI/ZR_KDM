ffc246bd1dfb0c78e3763e3115d4f4916732b2a1
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-09-19 01:29:27 -0300

    feat(zapador): Zapador ahora soporta configuración de usuario
    La configuración es almacenada y leída en un archivo .json que se genera la primera vez que se ejecuta

--- herramientas/zapador/main.py
+++ herramientas/zapador/main.py
@@ -11,28 +11,36 @@ import zipfile
 import time
 import shutil
 from pathlib import Path
-import generador_mision 
+import generador_mision
+import json
 
 # globales
+primera_vez         = True
 url                 = "https://github.com/ZR-TECDI/ZR_KDM/archive/master.zip"
 quiere_crear        = False
 dir_script          = os.path.dirname(os.path.realpath(__file__))
 directorio_descarga = dir_script + "\\descargas"
-MPMISSIONS_DIR = ""
+
+CONFIG_DICT = {
+    'primera_vez': 'false',
+    'mpmissions' : '',
+    'online' : 'True'
+}
+
+CONFIG_ONLINE       = ""
+MPMISSIONS_DIR      = ""
 
 main_menu = """
     +=======================+===============================================================================+
     |        Comando        |                                  Descripción                                  |
     +=======================+===============================================================================+
-    | 1- Setear mpmissions  | Guarda la ruta a tu carpeta de misiones                                       |
+    | 1- Configurar KDM     | Cambia el comportamiento de KDM                                               |
     +-----------------------+-------------------------------------------------------------------------------+
-    | 2- Actualizar KDM     | Descarga la última versión estable de KDM y la mueve a tu carpeta de misiones |
+    | 2- Crear misión       | Mueve KDM a tu carpeta de misiones y crea archivos base por ti                |
     +-----------------------+-------------------------------------------------------------------------------+
-    | 3- Actualizar y crear | Lo mismo que la anterior, pero también crea archivos para tu nueva misión     |
+    | 3- Olvidar seteo      | Reinicia la configuración de Zapador                                          |
     +-----------------------+-------------------------------------------------------------------------------+
-    | 4- Olvidar seteo      | Olvida la dirección de tu carpeta de misiones                                 |
-    +-----------------------+-------------------------------------------------------------------------------+
-    | 5- Salir              | Cierra la aplicación                                                          |
+    | 4- Salir              | Cierra la aplicación                                                          |
     +-----------------------+-------------------------------------------------------------------------------+
 """
 sepa = """   .     .     .     .     .     .     .     .     .     .
@@ -50,27 +58,41 @@ EXT_MAPA      = '.VR'
 def descarga():
     """Descarga la última versión disponible del KDM en un directorio nuevo llamado 'descargas'"""
 
-    print(sepa)
-    print("Descargando última versión estable desde: "+ url)
+    if quiere_crear:
 
-    try:
-        os.mkdir(dir_script + "\\descargas")
-    except Exception as e:
-        print("ERROR AL CREAR DIRECTORIO DE DESCARGA: " + str(e))
-        os.system("pause")
-    else:
-        print("Directorio de descarga temporal creado con éxito")
-        os.chdir(dir_script + "\\descargas")
+        print(sepa)
+        print("Descargando última versión estable desde: "+ url)
 
-    try:
-        ultimo_estable = urllib.request.urlretrieve(url, directorio_descarga+"\\kdm.zip")
-    except Exception as e:
-        print("ERROR AL DESCARGAR: "+ str(e))
-        os.system("pause")
+        try:
+            os.mkdir(dir_script + "\\descargas")
+        except Exception as e:
+            print("ERROR AL CREAR DIRECTORIO DE DESCARGA: " + str(e))
+            os.system("pause")
+        else:
+            print("Directorio de descarga temporal creado con éxito")
+            os.chdir(dir_script + "\\descargas")
+
+        try:
+            ultimo_estable = urllib.request.urlretrieve(url, directorio_descarga+"\\kdm.zip")
+        except Exception as e:
+            print("ERROR AL DESCARGAR: "+ str(e))
+            os.system("pause")
+        else:
+            ultimo_estable = ultimo_estable[0]
+            print("Archivo descargado con éxito en: "+ultimo_estable)
+            unzip(ultimo_estable)
     else:
-        ultimo_estable = ultimo_estable[0]
-        print("Archivo descargado con éxito en: "+ultimo_estable)
-        unzip(ultimo_estable)
+        print(sepa)
+        print('Modo ofline detectado, necesitarás una copia de KDM junto a este script para continuar...')
+        plantilla = dir_script + "\\template_ZR.VR"
+        ruta_plantilla = Path(plantilla)
+        if ruta_plantilla.exists():
+            mover_carpeta(plantilla)
+        else:
+            print('No encontramos una copia válida de KDM junto a este script.')
+            print('Si no posees una, cambia el modo online a True en el menú de cambiar configuración de Zapador.')
+            os.system('pause')
+            main()
 
 
 def unzip(archivo):
@@ -179,7 +201,7 @@ def crear_archivos(carpeta_final):
 
     EXT_MAPA = input("El número de tu elección:\n> ")
 
-    if EXT_MAPA == 1:
+    if EXT_MAPA == '1':
         EXT_MAPA = '.Altis'
     elif EXT_MAPA == '2':
         EXT_MAPA = ".Stratis"
@@ -239,51 +261,72 @@ def crear_archivos(carpeta_final):
         EXT_MAPA = '.zargabad'
     else:
         print('Error, la opción elegida no es válida. Usando valor DEFAULT')
+        EXT_MAPA = '.VR'
         os.system('pause')
 
     generador_mision.main(carpeta_final, AUTOR, NOMBRE_MISION, quiere_foto, FOTO_MISION, DESC_MISION, EXT_MAPA, MPMISSIONS_DIR)
 
 
-def set_mpmission():
+def set_configuracion():
     """Crea o sobrescribe el archivo desde el que se toma la ruta a la carpeta de misiones"""
 
     global MPMISSIONS_DIR
+    global CONFIG_ONLINE
+    global CONFIG_DICT
+
     os.system("cls")
-    print("Busca la ruta a tu carpeta de misiones, debería verse algo así: ")
-    print("C:\TU-USUARIO\Documents\Arma 3 - Other Profiles\TU-PERFIL-DE-ARMA\mpmissions")
+    with open(dir_script + '\zapador_config.json', 'r') as fp:
+        CONFIG_DICT = json.load(fp)
+
+    MPMISSIONS_DIR = CONFIG_DICT['mpmissions']
+    CONFIG_ONLINE = CONFIG_DICT['online']
+
+    print("1. Carpeta de misión : {}".format(MPMISSIONS_DIR))
+    print("2. Descargar siempre última versión : {}".format(CONFIG_ONLINE))
+    print("0. Volver al menú principal")
     print()
-    MPMISSIONS_DIR = input("Ingresa la ruta ahora: ")
-    with open(dir_script+"/mpmissions.txt", 'w') as f:
-        f.write(MPMISSIONS_DIR)
-    main()
+    print("¿Qué configuración quieres cambiar?")
+    choice = input("> ")
+
+    if choice == "0":
+        main()
+    elif choice == "1":
+        value = input("Ingrese nuevo valor para esta configuración: \n>")
+        CONFIG_DICT['mpmissions'] = value
+        with open (dir_script + '\zapador_config.json', 'w') as fp:
+            json.dump(CONFIG_DICT, fp)
+    elif choice == "2":
+        value = input("Ingrese nuevo valor para esta configuración: \n>")
+        CONFIG_DICT['online'] = value
+        with open (dir_script + '\zapador_config.json', 'w') as fp:
+            json.dump(CONFIG_DICT, fp)
+    
+    os.system('cls')
+    print("Configuración actualizada...")
+    os.system('pause')
+    set_configuracion()
 
 
-def olvidar_mpmissions():
+def olvidar_configuracion():
     """Elimina el archivo desde donde se toma la carpeta de misiones"""
 
     os.system("cls")
     try:
-        os.remove(dir_script + "/mpmissions.txt")
+        os.remove(dir_script + "/zapador_config.json")
     except Exception as e:
-        print("ERROR AL INTENTAR ELIMINAR DATO: ")
+        print("ERROR AL INTENTAR ELIMINAR CONFIGURACIÓN: ")
         print(str(e))
     else:
-        print("Dato eliminado con éxito")
+        print("Configuración eliminada con éxito")
     finally:
         os.system("pause")
-
+    
+    leer_configuracion()
 
 def intro():
     """Presenta la aplicación con su versión y créditos"""
 
-    global MPMISSIONS_DIR
-
-    try: 
-        with open(dir_script + "/mpmissions.txt", 'r') as f:
-            MPMISSIONS_DIR = f.readline()
-    except:
-        pass
-
+    os.system('cls')
     print()
     print("""
                        _____________________________________________________
@@ -302,15 +345,44 @@ def intro():
 
     """)
     print()
-    if MPMISSIONS_DIR == "":
-        print("Carpeta de misión sin setear")
-    else:
-        print("Carpeta de misión: "+ MPMISSIONS_DIR)
-    time.sleep(2)
-    os.system("pause")
-    os.system("cls")
+    os.system('pause')
     main()
 
+def leer_configuracion():
+
+    global MPMISSIONS_DIR
+    global primera_vez
+    global CONFIG_DICT
+
+    try: 
+        with open(dir_script + "/zapador_config.json", 'r') as f:
+            CONFIG_DICT = json.load(f)
+            primera_vez = CONFIG_DICT['primera_vez']
+    except:
+        primera_vez = True
+    else:
+        primera_vez = False
+
+    if primera_vez:
+        os.system('cls')
+        print("Bienvenido a Zapador.\n")
+        print("Este programa fue diseñado con la intención de facilitar el pie inicial a la creación de misiones.")
+        print("A continuación, necesitamos que nos des algo de información para configurar todo.\n")
+        print()
+        print("Busca la ruta a tu carpeta de misiones, debería verse algo así: ")
+        print("C:\\Users\TU-USUARIO\Documents\Arma 3 - Other Profiles\TU-PERFIL-DE-ARMA\mpmissions")
+        MPMISSIONS_DIR = input("Ingresa la ruta ahora: ")
+        CONFIG_DICT['mpmissions'] = MPMISSIONS_DIR
+        primera_vez = False
+        data = json.dumps(CONFIG_DICT)
+
+        with open (dir_script+'\zapador_config.json', 'w')as f:
+            f.write(data)
+            intro()
+        CONFIG_DICT = json.loads(data)
+
+    else:
+        intro()
 
 def main():
     """Menú principal de la aplicación"""
@@ -325,17 +397,18 @@ def main():
         choice = input("> ")
 
         if choice == "1":
-            set_mpmission()
+            set_configuracion()
         if choice == "2":
+            if CONFIG_DICT['online'] == "True":
+                quiere_crear = True
+            else:
+                quiere_crear = False
             descarga()
         if choice == "3":
-            quiere_crear = True
-            descarga()
+            olvidar_configuracion()
         if choice == "4":
-            olvidar_mpmissions()
-        if choice == "5":
             loop = False
             quit()
 
 if __name__ == "__main__":
-	intro()
+	leer_configuracion()
